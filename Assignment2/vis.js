a721dc1b21c5ca8a71867248f2e309d1aa509f72
Martin Metaksov <info@metaksov.dk>
2017-12-05 16:35:55 +0100

    added pie visualizations for the 3 different cities

--- Assignment2/vis.js
+++ Assignment2/vis.js
@@ -19,6 +19,8 @@ function plotGraphs(station) {
             plotMinMax(data, station);
 
             plotSeasons(data, station);
+
+            plotMonths(data, station);
         });
 }
 
@@ -293,6 +295,118 @@ function plotSeasons(data, station) {
             else
                 return 'September - October - November'
         });
+}
+
+function plotMonths(data, station) {
+
+    let svg = d3.select('#plot-months-' + station);
+
+    if (svg.empty()) return;
+
+    let margin = {top: 10, right: 10, bottom: 10, left: 10},
+        width = +svg.attr('width'),
+        height = +svg.attr('height');
+
+    let g = svg.attr("width", (width + margin.left + margin.right))
+        .attr("height", (height + margin.top + margin.bottom))
+        .append("g").attr("class", "wrapper")
+        .attr("transform", "translate(" + (width / 2 + margin.left) + "," + (height / 2 + margin.top) + ")");
+
+
+    // prepare the data
+    let filteredData = data.columns.slice(1, 13).map(function (r) {
+        let monthly_data = data.map(d => d[r]);
+        let min = d3.min(monthly_data);
+        let max = d3.max(monthly_data);
+        let mean = d3.mean(monthly_data);
+        return {"name": r,"min": min, "max": max, "mean": mean};
+    });
+
+    //Create a color scale
+    let colorScale = d3.scaleLinear()
+        .domain([-40, 0, 4, 8, 12, 16, 20, 40])
+        .range(["#2c7bb6", "#00a6ca","#00ccbc","#90eb9d","#ffff8c","#f9d057","#f29e2e","#e76818","#d7191c"])
+        .interpolate(d3.interpolateHcl);
+
+    //Create an arc function
+    let arc = d3.arc()
+        .innerRadius(width*0.15/2)
+        .outerRadius(width*0.3/2 + 30);
+
+    //Turn the pie chart 90 degrees counter clockwise, so it starts at the left
+    let pie = d3.pie()
+        .startAngle(-90 * Math.PI/180)
+        .endAngle(-90 * Math.PI/180 + 2*Math.PI)
+        .value(1)
+        .padAngle(.01)
+        .sort(null);
+
+    //Create the donut slices and also the invisible arcs for the text
+    g.selectAll(".donutArcs")
+        .data(pie(filteredData))
+        .enter().append("path")
+        .attr("class", "donutArcs")
+        .attr("d", arc)
+        .style("fill", function(d,i) {
+            return colorScale(d.data.mean);
+        })
+        .each(function(d,i) {
+            //Search pattern for everything between the start and the first capital L
+            let firstArcSection = /(^.+?)L/;
+
+            //Grab everything up to the first Line statement
+            let newArc = firstArcSection.exec( d3.select(this).attr("d") )[1];
+            //Replace all the comma's so that IE can handle it
+            newArc = newArc.replace(/,/g , " ");
+
+            //If the end angle lies beyond a quarter of a circle (90 degrees or pi/2)
+            //flip the end and start position
+            if (d.endAngle > 90 * Math.PI/180) {
+                let startLoc 	= /M(.*?)A/,		//Everything between the first capital M and first capital A
+                    middleLoc 	= /A(.*?)0 0 1/,	//Everything between the first capital A and 0 0 1
+                    endLoc 		= /0 0 1 (.*?)$/;	//Everything between the first 0 0 1 and the end of the string (denoted by $)
+                //Flip the direction of the arc by switching the start en end point (and sweep flag)
+                //of those elements that are below the horizontal line
+                let newStart = endLoc.exec( newArc )[1];
+                let newEnd = startLoc.exec( newArc )[1];
+                let middleSec = middleLoc.exec( newArc )[1];
+
+                //Build up the new arc notation, set the sweep-flag to 0
+                newArc = "M" + newStart + "A" + middleSec + "0 0 0 " + newEnd;
+            }//if
+
+            //Create a new invisible arc that the text can flow along
+            svg.append("path")
+                .attr("class", "hiddenDonutArcs")
+                .attr("id", "donutArc"+i)
+                .attr("d", newArc)
+                .style("fill", "none");
+        })
+        .append("svg:title")
+        .html(d =>
+            "<span>Min: " + d.data.min + "&deg;</span>" +
+            "<br>" +
+            "<span>Max: " + d.data.max + "&deg;</span>" +
+            "<br>" +
+            "<span>Mean: " + temperatureFormat(d.data.mean) + "&deg;</span>"
+        );
+
+    //Append the label names on the outside
+    g.selectAll(".donutText")
+        .data(pie(filteredData))
+        .enter().append("text")
+        .attr("class", "donutText")
+        //Move the labels below the arcs for those slices with an end angle greater than 90 degrees
+        .attr("dy", function(d,i) { return (d.endAngle > 90 * Math.PI/180 ? 18 : -11); })
+        .append("textPath")
+        .attr("startOffset","50%")
+        .style("text-anchor","middle")
+        .attr("xlink:href",function(d,i){return "#donutArc"+i;})
+        .text(function(d){return d.data.name;});
+
+
+
+
 }
 
 // returns slope, intercept and r-square of the line
