0430bfe36c7b92b594228b9ec88eff922dd0c16e
Marek Hlava <headsvk@gmail.com>
2017-12-12 01:04:37 +0100

    added twitter pictures, made the graph larger

--- Assignment3/vis.js
+++ Assignment3/vis.js
@@ -1,52 +1,79 @@
 d3.select(window).on('load', loadData);
 
-let temperatureFormat = d3.format('0.1f');
+function getTwitterAvatar(handle) {
+    return 'https://twitter.com/' + handle + '/profile_image?size=bigger';
+}
 
 function loadData() {
-    // let x = 'example';
-    let x = 'trump_family';
     d3.json(
-        'data/' + x + '.json',
+        'data/trump_family.json',
         (error, data) => {
             if (error) throw error;
-            init(data);
+
+            init(data, extractLinearData(data));
         });
 }
 
-function init(treeData) {
-    // Set the dimensions and margins of the diagram
-    let margin = {top: 20, right: 90, bottom: 30, left: 90},
-        width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
+function extractLinearData(treeData) {
 
-    let colorScale = d3.scaleLinear()
+    const node = {'name': treeData.name, 'twitter': treeData.twitter};
+
+    const innerNodes = treeData.partners ? treeData.partners
+        : (treeData.children ? treeData.children : []);
+
+    return [node].concat(...innerNodes.map(node => extractLinearData(node)));
+}
+
+function init(data, linearData) {
+
+    const radius = 25;
+
+    const colorScale = d3.scaleLinear()
         .domain([0, 1])
         .range(['red', 'green']);
-    let widthScale = d3.scaleLinear()
-        .domain([1,80])
+    const widthScale = d3.scaleLinear()
+        .domain([1, 80])
         .range([1, 10]);
 
     // append the svg object to the body of the page
-    // appends a 'group' element to 'svg'
-    // moves the 'group' element to the top left margin
-    let svg = d3.select("body").append("svg")
-        .attr("width", width + margin.right + margin.left)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate("
-            + margin.left + "," + margin.top + ")");
+    const svg = d3.select('#trump');
 
-    let i = 0,
-        duration = 750,
-        root;
+    // append picture definitions
+    svg.append('defs')
+        .selectAll('pattern')
+        .data(linearData.filter(d => d.twitter))
+        .enter()
+        .append('pattern')
+        .attr('id', d => 'image-' + d.twitter)
+        .attr('height', '100%')
+        .attr('width', '100%')
+        .attr('x', '0%')
+        .attr('y', '0%')
+        .attr('viewBox', '0 0 73 73')
+        .append('image')
+        .attr('xlink:href', d => getTwitterAvatar(d.twitter))
+        .attr('width', '73')
+        .attr('height', '73')
+        .attr('x', '0%')
+        .attr('y', '0%');
+
+    // append a 'group' element to 'svg'
+    // move the 'group' element to the top left margin
+    const group = svg.append('g')
+        .attr('transform', 'translate(' + 250 + ',' + -10 + ')');
+
+    let i = 0;
+    const duration = 750;
+
+    const svgElem = document.getElementById('trump');
+    const height = svgElem.clientHeight;
+    const width = svgElem.clientWidth;
 
     // declares a tree layout and assigns the size
-    let treemap = d3.tree().size([height, width]);
+    const treemap = d3.tree().size([height, width]);
 
     // Assigns parent, partners, height, depth
-    root = d3.hierarchy(treeData, function(d) {
-        return d.children ? d.children : d.partners;
-    });
+    const root = d3.hierarchy(data, d => d.children ? d.children : d.partners);
     root.x0 = height / 2;
     root.y0 = 0;
 
@@ -55,24 +82,26 @@ function init(treeData) {
     function update(source) {
 
         // Assigns the x and y position for the nodes
-        let treeData = treemap(root);
+        const treeData = treemap(root);
 
         // Compute the new tree layout.
-        let nodes = treeData.descendants(),
+        const nodes = treeData.descendants(),
             links = treeData.descendants().slice(1);
 
         // Normalize for fixed-depth.
-        nodes.forEach(function(d){ d.y = d.depth * 180});
+        nodes.forEach(d => {
+            d.y = d.depth * 180
+        });
 
         // ****************** Nodes section ***************************
 
         // Update the nodes...
-        let node = svg.selectAll('g.node')
-            .data(nodes, function(d) {return d.id || (d.id = ++i); });
+        const node = group.selectAll('g.node')
+            .data(nodes, d => d.id || (d.id = ++i));
 
         // Enter any new modes at the parent's previous position.
-        let nodeEnter = node.enter().append('g')
-            .attr('class', function(d) {
+        const nodeEnter = node.enter().append('g')
+            .attr('class', d => {
                 // return 'node';
                 if (isParent(d)) {
                     return 'node parent';
@@ -80,56 +109,42 @@ function init(treeData) {
                     return 'node';
                 }
             })
-            .attr("transform", function(d) {
-                return "translate(" + source.y0 + "," + source.x0 + ")";
-            })
+            .attr('transform', () => 'translate(' + source.y0 + ',' + source.x0 + ')')
             .on('click', click);
 
         // Add Circle for the nodes
         nodeEnter.append('circle')
             .attr('class', 'node')
             .attr('r', 1e-6)
-            .style("fill", function(d) {
-                return d._children ? "lightsteelblue" : "#fff";
-            })
-            .style("stroke", colorScale[5]);
+            .style('fill', d => d.data.twitter ? ('url(#image-' + d.data.twitter + ')') : 'lightgrey')
+            .style('stroke', colorScale[5]);
 
         // Add labels for the nodes
         nodeEnter.append('text')
-            .attr("dy", ".35em")
-            .attr("x", function(d) {
-                return d.children || d._children ? -13 : 13;
-            })
-            .attr("text-anchor", function(d) {
-                return d.children || d._children ? "end" : "start";
-            })
-            .text(function(d) { return d.data.name; });
+            .attr('dy', '.35em')
+            .attr('x', d => d.children || d._children ? -radius - 3 : radius + 3)
+            .attr('y', -10)
+            .attr('text-anchor', d => d.children || d._children ? 'end' : 'start')
+            .text(d => d.data.name);
 
         // UPDATE
-        let nodeUpdate = nodeEnter.merge(node);
+        const nodeUpdate = nodeEnter.merge(node);
 
         // Transition to the proper position for the node
         nodeUpdate.transition()
             .duration(duration)
-            .attr("transform", function(d) {
-                return "translate(" + d.y + "," + d.x + ")";
-            });
+            .attr('transform', d => 'translate(' + d.y + ',' + d.x + ')');
 
         // Update the node attributes and style
         nodeUpdate.select('circle.node')
-            .attr('r', 10)
-            .style("fill", function(d) {
-                return d._children ? "lightsteelblue" : "#fff";
-            })
+            .attr('r', radius)
             .attr('cursor', 'pointer');
 
 
         // Remove any exiting nodes
-        let nodeExit = node.exit().transition()
+        const nodeExit = node.exit().transition()
             .duration(duration)
-            .attr("transform", function(d) {
-                return "translate(" + source.y + "," + source.x + ")";
-            })
+            .attr('transform', () => 'translate(' + source.y + ',' + source.x + ')')
             .remove();
 
         // On exit reduce the node circles size to 0
@@ -143,9 +158,9 @@ function init(treeData) {
         // ****************** links section ***************************
 
         // Update the links...
-        let link = svg.selectAll('path.link')
-            .data(links, function(d) { return d.id; })
-            .style('stroke-width', function(d) {
+        const link = group.selectAll('path.link')
+            .data(links, d => d.id)
+            .style('stroke-width', d => {
                 if (isParent(d)) {
                     return 5;
                 } else {
@@ -155,17 +170,16 @@ function init(treeData) {
 
         function isParent(d) {
             return d.parent && 'partners' in d.parent.data;
-
         }
 
         // Enter any new links at the parent's previous position.
-        let linkEnter = link.enter().insert('path', "g")
-            .attr("class", "link")
-            .attr('d', function(d){
-                let o = {x: source.x0, y: source.y0};
+        const linkEnter = link.enter().insert('path', 'g')
+            .attr('class', 'link')
+            .attr('d', () => {
+                const o = {x: source.x0, y: source.y0};
                 return diagonal(o, o)
             })
-            .style('stroke-width', function(d) {
+            .style('stroke-width', d => {
                 if (isParent(d)) {
                     return 5;
                 } else {
@@ -174,27 +188,25 @@ function init(treeData) {
             });
 
         // UPDATE
-        let linkUpdate = linkEnter.merge(link);
+        const linkUpdate = linkEnter.merge(link);
 
         // Transition back to the parent element position
         linkUpdate.transition()
             .duration(duration)
-            .attr('d', function(d){ return diagonal(d, d.parent) });
+            .attr('d', d => diagonal(d, d.parent));
 
         // Remove any exiting links
-        let linkExit = link.exit().transition()
+        link.exit().transition()
             .duration(duration)
-            .attr('d', function(d) {
-                let o = {x: source.x, y: source.y};
+            .attr('d', () => {
+                const o = {x: source.x, y: source.y};
                 return diagonal(o, o)
             })
-            .style('stroke-width', function(d){
-                return widthScale(d.data.value)
-            })
+            .style('stroke-width', d => widthScale(d.data.value))
             .remove();
 
         // Store the old positions for transition.
-        nodes.forEach(function(d){
+        nodes.forEach(d => {
             d.x0 = d.x;
             d.y0 = d.y;
         });
