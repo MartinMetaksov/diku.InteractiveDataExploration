7e5a0c1c9b1ef2d7b926659e36a02e40b515dd1d
Martin Metaksov <info@metaksov.dk>
2017-12-25 02:29:33 +0100

    created a map of san francisco police districts based on the sfpd_crime.geojson, which I tranformed into a topojson file for performance reasons; added some general front-end optimizations (added bootstrap, improved css)

--- Assignment5/vis.js
+++ Assignment5/vis.js
@@ -7,12 +7,54 @@ function zip(a, b) {
 }
 
 function loadData() {
-    d3.text('data/sf_crime.geojson', (error1, data) => {
+    d3.json('data/sfpd_crime.topojson', (error1, data) => {
         if (error1) throw error1;
         init(data);
     });
 }
 
 function init(data) {
+    let jq_map = $('#map'),
+        width = jq_map.width(),
+        height = jq_map.height();
+
+    let svg = d3.select("#map");
+
+    let color = d3.scaleLinear()
+        .domain([0, 9])
+        .clamp(true)
+        .range(['#F0F8FF', '#002147']);
+
+    /*
+    * Mercator is a projection type
+    * https://en.wikipedia.org/wiki/Mercator_projection
+    */
+    let projection = d3.geoMercator()
+        .center([-122.433701, 37.767683])
+        // .scale(width / 2 / Math.PI)
+        .scale(500)
+        .translate([width / 2, height / 2]);
+
+    let path = d3.geoPath()
+        .projection(projection);
+
+    let plane = topojson.feature(data, data.objects.collection);  <!-- read data -->
+
+    projection.scale(1).translate([0, 0]);
+    let b = path.bounds(plane);
+    let s = .9 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height);
+    let t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2];
+    projection.scale(s).translate(t);
+
+    vector= svg.selectAll("path")
+        .data(plane.features)
+        .enter()
+        .append("path")
+        .attr("class", "link")
+        .attr("id", (_, i) => "id-"+i)
+        .attr('data-toggle', 'tooltip')
+        .attr('title', (_, i) => 'District ' + i)
+        .attr("d", path)
+        .attr('fill', (_, i) => color(i));
 
 }
