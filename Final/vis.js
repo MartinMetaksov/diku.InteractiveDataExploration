22ad9b003ffe27f4361d25189c436a95dd12dd3c
Martin Metaksov <info@metaksov.dk>
2018-01-05 23:48:05 +0100

    a very important part of the assignment

--- Final/vis.js
+++ Final/vis.js
@@ -1,10 +1,19 @@
 d3.select(window).on('load', loadData);
 
+$(document).ready(function() {
+    $(window).scroll($.debounce( 250, true, function(){
+        $('#ufo-image').attr('src', 'ufos/ufo_flying.svg');
+    }));
+    $(window).scroll($.debounce( 250, function(){
+        $('#ufo-image').attr('src', 'ufos/ufo_abduct.svg');
+    }));
+});
+
 function loadData() {
-    d3.json('data/sfpd_crime.topojson', (error, data) => {
-        if (error) throw error;
-        init(data);
-    });
+    // d3.json('data/sfpd_crime.topojson', (error, data) => {
+    //     if (error) throw error;
+    //     init(data);
+    // });
 }
 
 function init(data) {
