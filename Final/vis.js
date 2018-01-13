33aec610d82f33cc34cbfe42f02faf977c7340ca
Marek Hlava <headsvk@gmail.com>
2018-01-13 18:23:02 +0100

    added basic world map

--- Final/vis.js
+++ Final/vis.js
@@ -11,6 +11,7 @@ $(document).ready(() => {
 
 function init() {
     plotVisualizations('scrubbed');
+    initMap();
 }
 
 function plotVisualizations(db) {
@@ -286,4 +287,206 @@ function plotUfosByYear(data) {
         .attr("y", d => y(d.count))
         .attr("width", x.bandwidth())
         .attr("height", d => height - y(d.count))
+}
+
+// Source: http://techslides.com/d3-map-starter-kit
+function initMap() {
+
+    d3.select(window).on("resize", throttle);
+
+    let zoom = d3.zoom()
+    //.extent([1,9])
+        .scaleExtent([1, 9])
+        .on("zoom", move);
+
+    let c = document.getElementById('map-container');
+    let width = c.offsetWidth;
+    let height = width / 2;
+
+    //offsets for tooltips
+    let offsetL = c.offsetLeft+20;
+    let offsetT = c.offsetTop+10;
+
+    let topo,projection,path,svg,g;
+
+    //let graticule = d3.geo.graticule();
+    let graticule = d3.geoGraticule();
+
+    let tooltip = d3.select("#map-container").append("div").attr("class", "tooltip hidden");
+
+    setup(width,height);
+
+    function setup(width,height){
+        //projection = d3.geo.mercator()
+        projection = d3.geoMercator()
+            .translate([(width/2), (height/2)])
+            .scale( width / 2 / Math.PI);
+
+        //path = d3.geo.path().projection(projection);
+        path = d3.geoPath().projection(projection);
+
+        svg = d3.select("#map-container").append("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .call(zoom)
+            //.on("click", click)
+            .append("g");
+
+        g = svg.append("g")
+            .on("click", click);
+
+    }
+
+    d3.json("data/world-topo-min.json", function(error, world) {
+
+        topo = topojson.feature(world, world.objects.countries).features;
+
+        draw(topo);
+
+    });
+
+    function handleMouseOver(){
+        let mouse = d3.mouse(svg.node()).map( function(d) { return parseInt(d); } );
+
+        tooltip.classed("hidden", false)
+            .attr("style", "left:"+(mouse[0]+offsetL)+"px;top:"+(mouse[1]+offsetT)+"px")
+            .html(this.__data__.properties.name);
+    }
+
+    function handleMouseOut(){
+        tooltip.classed("hidden", true);
+    }
+
+
+    function draw(topo) {
+
+        svg.append("path")
+            .datum(graticule)
+            .attr("class", "graticule")
+            .attr("d", path);
+
+
+        g.append("path")
+            .datum({type: "LineString", coordinates: [[-180, 0], [-90, 0], [0, 0], [90, 0], [180, 0]]})
+            .attr("class", "equator")
+            .attr("d", path);
+
+
+        let country = g.selectAll(".country").data(topo);
+
+        country.enter().insert("path")
+            .attr("class", "country")
+            .attr("d", path)
+            .attr("id", function(d,i) { return d.id; })
+            .attr("title", function(d,i) { return d.properties.name; })
+            .style("fill", function(d, i) { return d.properties.color; })
+            .on("mouseover", handleMouseOver)
+            .on("mouseout", handleMouseOut);
+
+
+        //tooltips
+        /*
+        d3.select("#map-container svg path")
+          .on("mousemove", function(d,i) {
+      console.log(d);
+            let mouse = d3.mouse(svg.node()).map( function(d) { return parseInt(d); } );
+
+            tooltip.classed("hidden", false)
+                   .attr("style", "left:"+(mouse[0]+offsetL)+"px;top:"+(mouse[1]+offsetT)+"px")
+                   .html(d.properties.name);
+
+            })
+            .on("mouseout",  function(d,i) {
+              tooltip.classed("hidden", true);
+            });
+      */
+
+        //EXAMPLE: adding some capitals from external CSV file
+        d3.csv("data/country-capitals.csv", function(err, capitals) {
+
+            capitals.forEach(function(i){
+                addpoint(i.CapitalLongitude, i.CapitalLatitude, i.CapitalName );
+            });
+
+        });
+
+    }
+
+
+    function redraw() {
+        width = c.offsetWidth;
+        height = width / 2;
+        d3.select('svg').remove();
+        setup(width,height);
+        draw(topo);
+    }
+
+
+    function move() {
+
+        //let t = d3.event.translate;
+        let t = [d3.event.transform.x,d3.event.transform.y];
+        //let s = d3.event.scale;
+        let s = d3.event.transform.k;
+        zscale = s;
+        let h = height/4;
+
+        t[0] = Math.min(
+            (width/height)  * (s - 1),
+            Math.max( width * (1 - s), t[0] )
+        );
+
+        t[1] = Math.min(
+            h * (s - 1) + h * s,
+            Math.max(height  * (1 - s) - h * s, t[1])
+        );
+
+        //zoom.translateBy(t);
+        g.attr("transform", "translate(" + t + ")scale(" + s + ")");
+
+        //adjust the country hover stroke width based on zoom level
+        d3.selectAll(".country").style("stroke-width", 1.5 / s);
+
+    }
+
+    let throttleTimer;
+    function throttle() {
+        window.clearTimeout(throttleTimer);
+        throttleTimer = window.setTimeout(function() {
+            redraw();
+        }, 200);
+    }
+
+
+    //geo translation on mouse click in map
+    function click() {
+        let latlon = projection.invert(d3.mouse(this));
+        console.log(latlon);
+    }
+
+
+    //function to add points and text to the map (used in plotting capitals)
+    function addpoint(lon,lat,text) {
+
+        let gpoint = g.append("g").attr("class", "gpoint");
+        let x = projection([lon,lat])[0];
+        let y = projection([lon,lat])[1];
+
+        gpoint.append("svg:circle")
+            .attr("cx", x)
+            .attr("cy", y)
+            .attr("class","point")
+            .attr("r", 1.5);
+
+        //conditional in case a point has no associated text
+        if(text.length>0){
+
+            gpoint.append("text")
+                .attr("x", x+2)
+                .attr("y", y+2)
+                .attr("class","text")
+                .text(text);
+        }
+
+    }
 }
