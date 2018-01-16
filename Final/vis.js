59d5c56a5b1436491bc9174da956dff3f71e5fb1
alexLonch <lonchambon.alex@gmail.com>
2018-01-16 19:08:34 +0100

    add transition, plots, colors grading

--- Final/vis.js
+++ Final/vis.js
@@ -7,7 +7,10 @@ let ufoData,
     currentMoment,
     totalDuration,
     interval,
-    datetimeDimension;
+    datetimeDimension,
+    monthLetter = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+    dayLetter = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
 
 function init() {
     plotVisualizations('scrubbed');
@@ -23,6 +26,8 @@ function plotVisualizations(db) {
             data.forEach(d => {
                 d.dt = new Date(d.dt);
                 d.year = d.dt.getFullYear();
+                d.month = monthLetter[d.dt.getMonth()];
+                d.day = dayLetter[d.dt.getDay()];
                 d.duration_sec = +d.duration_sec || 0;
                 d.city = d.city.capitalize();
                 d.state = d.state.length <= 3 ? d.state.toUpperCase() : d.state.capitalize();
@@ -48,6 +53,12 @@ function plotUfos() {
             break;
         case "year":
             plotUfosByYear(ufoData);
+            break;
+        case "month":
+            plotUfosByMonth(ufoData);
+            break;
+        case "day":
+            plotUfosByDay(ufoData);
             break;
         default:
             plotUfosByShape(ufoData);
@@ -96,8 +107,7 @@ function plotUfosByShape(data) {
     let y = d3.scaleLinear().rangeRound([height, 0])
         .domain([0, d3.max(dataObj, d => d.count)]).nice();
 
-    let xAxis = d3.axisBottom(x)
-        .ticks(d3.datetime);
+    let xAxis = d3.axisBottom(x);
 
     let yAxis = d3.axisRight(y)
         .tickSize(width)
@@ -129,14 +139,13 @@ function plotUfosByShape(data) {
 
     let colors = d3.scaleOrdinal(d3.schemeCategory10);
     let tooltip = d3.select('body').append('div').attr('class', 'toolTip');
+
     g.selectAll('.bar')
         .data(dataObj)
         .enter().append('rect')
         .attr('class', 'bar')
         .attr('x', d => x(d.shape))
-        .attr('y', d => y(d.count))
-        .attr('width', x.bandwidth())
-        .attr('height', d => height - y(d.count))
+        .attr('y', height)
         .on('mousemove', d => {
             tooltip
                 .style('left', d3.event.pageX - 50 + 'px')
@@ -147,6 +156,14 @@ function plotUfosByShape(data) {
         .on('mouseout', () => {
             tooltip.style('display', 'none');
         })
+        .attr('width', x.bandwidth())
+        .style('fill', (d, i) => colors(i))
+        .attr('height', 0)
+        .transition()
+        .duration(200)
+        .delay((d,i) => i * 50)
+        .attr('y', d => y(d.count))
+        .attr('height', d => height - y(d.count))
         .style('fill', (d, i) => colors(i));
 }
 
@@ -225,16 +242,32 @@ function plotUfosByState(data) {
         .call(customYAxis);
 
     let colors = d3.scaleOrdinal(d3.schemeCategory20);
+    let tooltip = d3.select('body').append('div').attr('class', 'toolTip');
 
     g.selectAll('.bar')
         .data(dataObj)
         .enter().append('rect')
         .attr('class', 'bar')
         .attr('x', d => x(d.state))
-        .attr('y', d => y(d.count))
+        .attr('y', height)
+        .on('mousemove', d => {
+            tooltip
+                .style('left', d3.event.pageX - 50 + 'px')
+                .style('top', d3.event.pageY - 70 + 'px')
+                .style('display', 'inline-block')
+                .html((d.state) + '<br>' + (d.count) + ' UFOs');
+        })
+        .on('mouseout', () => {
+            tooltip.style('display', 'none');
+        })
         .attr('width', x.bandwidth())
-        .attr('height', d => height - y(d.count))
-        .style('fill', (d, i) => colors(i));
+        .style('fill', (d, i) => colors(i))
+        .attr('height', 0)
+        .transition()
+        .duration(200)
+        .delay((d,i) => i * 50)
+        .attr('y', d => y(d.count))
+        .attr('height', d => height - y(d.count));
 }
 
 function plotUfosByYear(data) {
@@ -306,15 +339,238 @@ function plotUfosByYear(data) {
     g.append('g')
         .call(customYAxis);
 
+    let colorScale = d3.scaleQuantile()
+        .domain([0, d3.max(dataObj, d => d.count)])
+        .range(d3.schemeCategory20);
+    let tooltip = d3.select('body').append('div').attr('class', 'toolTip');
+
     g.selectAll('.bar')
         .data(dataObj)
         .enter().append('rect')
         .attr('class', 'bar')
         .attr('x', d => x(d.dt))
-        .attr('y', d => y(d.count))
+        .attr('y', height)
+        .on('mousemove', d => {
+            tooltip
+                .style('left', d3.event.pageX - 50 + 'px')
+                .style('top', d3.event.pageY - 70 + 'px')
+                .style('display', 'inline-block')
+                .html((d.dt) + '<br>' + (d.count) + ' UFOs');
+        })
+        .on('mouseout', () => {
+            tooltip.style('display', 'none');
+        })
         .attr('width', x.bandwidth())
+        .attr('height', 0)
+        .transition()
+        .duration(200)
+        .delay((d,i) => i * 50)
+        .attr('y', d => y(d.count))
         .attr('height', d => height - y(d.count))
+        .style("fill", (d,i) => colorScale(d.count));
 }
+
+function plotUfosByMonth(data) {
+
+    d3.select("#plot-ufos-title").html("UFOs by month");
+
+    let svg = d3.select('#plot-ufos');
+    svg.selectAll("*").remove();
+
+    let counts = {};
+
+    data.forEach(d => {
+        if (!counts[d.month]) {
+            counts[d.month] = 0;
+        }
+        counts[d.month]++;
+    });
+
+    let dataObj = [];
+
+    Object.keys(counts).forEach(key => {
+        dataObj.push({
+            dt: key,
+            count: counts[key]
+        });
+    });
+
+    let margin = {top: 20, right: 20, bottom: 60, left: 40},
+        width = +svg.attr('width') - margin.left - margin.right,
+        height = +svg.attr('height') - margin.top - margin.bottom;
+
+    let g = svg.append('g')
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+    let x = d3.scaleBand().rangeRound([0, width]).padding(0.1)
+        .domain(dataObj.map(d => d.dt));
+
+    let y = d3.scaleLinear().rangeRound([height, 0])
+        .domain([0, d3.max(dataObj, d => d.count)]).nice();
+
+    let xAxis = d3.axisBottom(x)
+        .ticks(d3.dt);
+
+    let yAxis = d3.axisRight(y)
+        .tickSize(width)
+        .tickFormat(function (d) {
+            return this.parentNode.nextSibling
+                ? '\xa0' + d
+                : d + ' count';
+        });
+
+    function customXAxis(g) {
+        g.call(xAxis);
+        g.select('.domain').remove();
+        g.selectAll('.tick text').attr('transform', 'rotate(0)').attr('y', 6).attr('dy', '1.9em').attr('dx', '0em');
+    }
+
+    function customYAxis(g) {
+        g.call(yAxis);
+        g.select('.domain').remove();
+        g.selectAll('.tick:not(:first-of-type) line').attr('stroke', '#000000').attr('stroke-dasharray', '2,2');
+        g.selectAll('.tick text').attr('x', -20).attr('dy', -4);
+    }
+
+    g.append('g')
+        .attr('transform', 'translate(0,' + height + ')')
+        .call(customXAxis);
+
+    g.append('g')
+        .call(customYAxis);
+
+    let colorScale = d3.scaleQuantile()
+        .domain([0, d3.max(dataObj, d => d.count)])
+        .range(d3.schemeCategory10);
+    let tooltip = d3.select('body').append('div').attr('class', 'toolTip');
+
+    g.selectAll('.bar')
+        .data(dataObj)
+        .enter().append('rect')
+        .attr('class', 'bar')
+        .attr('x', d => x(d.dt))
+        .attr('y', height)
+        .on('mousemove', d => {
+            tooltip
+                .style('left', d3.event.pageX - 50 + 'px')
+                .style('top', d3.event.pageY - 70 + 'px')
+                .style('display', 'inline-block')
+                .html((d.dt) + '<br>' + (d.count) + ' UFOs');
+        })
+        .on('mouseout', () => {
+            tooltip.style('display', 'none');
+        })
+        .attr('width', x.bandwidth())
+        .attr('height', 0)
+        .transition()
+        .duration(200)
+        .delay((d,i) => i * 50)
+        .attr('y', d => y(d.count))
+        .attr('height', d => height - y(d.count))
+        .style("fill", (d,i) => colorScale(d.count));
+}
+
+function plotUfosByDay(data) {
+
+    d3.select("#plot-ufos-title").html("UFOs by day");
+
+    let svg = d3.select('#plot-ufos');
+    svg.selectAll("*").remove();
+
+    let counts = {};
+
+    data.forEach(d => {
+        if (!counts[d.day]) {
+            counts[d.day] = 0;
+        }
+        counts[d.day]++;
+    });
+
+    let dataObj = [];
+
+    Object.keys(counts).forEach(key => {
+        dataObj.push({
+            dt: key,
+            count: counts[key]
+        });
+    });
+
+    let margin = {top: 20, right: 20, bottom: 60, left: 40},
+        width = +svg.attr('width') - margin.left - margin.right,
+        height = +svg.attr('height') - margin.top - margin.bottom;
+
+    let g = svg.append('g')
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+    let x = d3.scaleBand().rangeRound([0, width]).padding(0.1)
+        .domain(dataObj.map(d => d.dt));
+
+    let y = d3.scaleLinear().rangeRound([height, 0])
+        .domain([0, d3.max(dataObj, d => d.count)]).nice();
+
+    let xAxis = d3.axisBottom(x)
+        .ticks(d3.dt);
+
+    let yAxis = d3.axisRight(y)
+        .tickSize(width)
+        .tickFormat(function (d) {
+            return this.parentNode.nextSibling
+                ? '\xa0' + d
+                : d + ' count';
+        });
+
+    function customXAxis(g) {
+        g.call(xAxis);
+        g.select('.domain').remove();
+        g.selectAll('.tick text').attr('transform', 'rotate(0)').attr('y', 6).attr('dy', '1.9em').attr('dx', '0em');
+    }
+
+    function customYAxis(g) {
+        g.call(yAxis);
+        g.select('.domain').remove();
+        g.selectAll('.tick:not(:first-of-type) line').attr('stroke', '#000000').attr('stroke-dasharray', '2,2');
+        g.selectAll('.tick text').attr('x', -20).attr('dy', -4);
+    }
+
+    g.append('g')
+        .attr('transform', 'translate(0,' + height + ')')
+        .call(customXAxis);
+
+    g.append('g')
+        .call(customYAxis);
+
+    let colorScale = d3.scaleQuantile()
+        .domain([0, d3.max(dataObj, d => d.count)])
+        .range(d3.schemeCategory20);
+    let tooltip = d3.select('body').append('div').attr('class', 'toolTip');
+
+    g.selectAll('.bar')
+        .data(dataObj)
+        .enter().append('rect')
+        .attr('class', 'bar')
+        .attr('x', d => x(d.dt))
+        .attr('y', height)
+        .on('mousemove', d => {
+            tooltip
+                .style('left', d3.event.pageX - 50 + 'px')
+                .style('top', d3.event.pageY - 70 + 'px')
+                .style('display', 'inline-block')
+                .html((d.dt) + '<br>' + (d.count) + ' UFOs');
+        })
+        .on('mouseout', () => {
+            tooltip.style('display', 'none');
+        })
+        .attr('width', x.bandwidth())
+        .attr('height', 0)
+        .transition()
+        .duration(200)
+        .delay((d,i) => i * 50)
+        .attr('y', d => y(d.count))
+        .attr('height', d => height - y(d.count))
+        .style("fill", (d,i) => colorScale(d.count));
+}
+
+
 
 // Source: http://techslides.com/d3-map-starter-kit
 function initMap(data) {
